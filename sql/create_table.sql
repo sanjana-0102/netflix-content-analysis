d3a5fe83ce6a34629dbbeb792a8b0812cdd339c1
sanjana-0102 <sanjanaraghu336@gmail.com>
2026-09-24 21:25:44 +0530

    Create netflix table with relevant columns

---
+++ sql/create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE netflix (
+    show_id VARCHAR(20),
+    type VARCHAR(20),
+    title TEXT,
+    director TEXT,
+    cast_members TEXT,
+    country TEXT,
+    date_added VARCHAR(50),
+    release_year INT,
+    rating VARCHAR(20),
+    duration VARCHAR(20),
+    listed_in TEXT,
+    description TEXT
+);
